22a0b2c9a2699ca960b8b0414d7d06824a0eb6be
Sylwek <sylwek185@op.pl>
2024-09-03 12:35:24 +0200

    fixes some values

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
+import { StrictMode } from 'react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-
-// import { StrictMode } from 'react';  // Dodaj ten import
-
 createRoot(document.getElementById('root')).render(
-	// <StrictMode>
-	<App />
-	// </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
